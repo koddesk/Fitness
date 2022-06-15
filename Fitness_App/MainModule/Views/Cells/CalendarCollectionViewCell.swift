33eeee8911f3ad41b36ed63936654add52f93518
Nikolay Dobychin <imkoddi@mail.ru>
2022-06-15 19:55:33 +0300

    Создание ячеек

---
+++ Fitness_App/MainModule/Views/Cells/CalendarCollectionViewCell.swift
@@ -0,0 +1,62 @@
+//
+//  CalendarCollectionViewCell.swift
+//  Fitness_App
+//
+//  Created by KODDER on 14.06.2022.
+//
+
+import UIKit
+
+class CalendarCollectionViewCell: UICollectionViewCell {
+    
+    let dayOfWeekLabel: UILabel = {
+       let label = UILabel()
+        label.text = "We"
+        label.font = .robotoBold16()
+        label.textColor = .white
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let numberOfDayLabel: UILabel = {
+       let label = UILabel()
+        label.text = "07"
+        label.font = .robotoBold20()
+        label.textColor = .white
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        layer.cornerRadius = 10
+        
+        addSubview(dayOfWeekLabel)
+        addSubview(numberOfDayLabel)
+    }
+    
+    private func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+            dayOfWeekLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            dayOfWeekLabel.topAnchor.constraint(equalTo: topAnchor, constant: 7)
+        ])
+        
+        NSLayoutConstraint.activate([
+            numberOfDayLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            numberOfDayLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5)
+        ])
+    }
+}
